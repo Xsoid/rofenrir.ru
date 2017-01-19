3dc0261c8afe174e849012913fe069208da8d185
Xsoid <xsoid@ya.ru>
2017-01-19 14:48:43 +0400

    fix

--- views/site/pages/ru/index.php
+++ views/site/pages/ru/index.php
@@ -12,104 +12,35 @@ $this->registerMetaTag([
      'content' => 'RoFenrir -  русский экспериментальный сервер Ragnarok Online. Бесплатный: без абонентской платы и внутренних платежей.'
 ]);
 ?>
+<div class="site-index">
 
-<h1>Поддержка проекта</h1>
+    <div class="jumbotron">
+        <h1>RO Fenrir</h1>
 
-<p>Проект полностью некоммерческий. Никакой абонентской платы или внутренних покупок. Оказанная материальная поддержка идет на развитие проекта.  Принимаются только добровольные пожертвования от лиц которым небезразличен наш общий мир. <a href="http://www.rofenrir.ru/news/podderzhka_proekta">Подробности</a></p>
+        <p class="lead">Ragnarok Online Server</p>
 
-<iframe frameborder="0" allowtransparency="true" scrolling="no" src="https://money.yandex.ru/embed/donate.xml?account=41001537616041&quickpay=donate&payment-type-choice=on&default-sum=210&targets=%D0%9F%D0%BE%D0%B4%D0%B4%D0%B5%D1%80%D0%B6%D0%BA%D0%B0+%D0%BF%D1%80%D0%BE%D0%B5%D0%BA%D1%82%D0%B0+RoFenrir&target-visibility=on&project-name=RoFenrir&project-site=http%3A%2F%2Fwww.rofenrir.ru&button-text=05&comment=on&hint=%D0%9D%D0%B0%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%80%3A+%22%D0%92%D0%BA%D0%BB%D0%B0%D0%B4+%D0%B2+%D0%BF%D0%B5%D1%80%D0%B5%D0%B2%D0%BE%D0%B4+%D0%BA%D0%B2%D0%B5%D1%81%D1%82%D0%BE%D0%B2+%D0%B2+%D0%9F%D1%80%D0%BE%D0%BD%D1%82%D0%B5%D1%80%D0%B5%22&fio=on&mail=on&successURL=http%3A%2F%2Fwww.rofenrir.ru%2Fabout%2Fserver-Support%2Fthanks" width="510" height="201"></iframe>
+        <p><a class="btn btn-lg btn-success" href="http://cp.rofenrir.ru/">Начать игру</a></p>
+    </div>
 
-<p>Спасибо за вашу поддержку.</p>
-<p>При возникновении вопросов связаться с администрацией можно по электронной почте fenrir@rofenrir.ru</p>
-<hr />
-<table class="table table-hover">
-    <caption>Люди которым мы безмерно благодарны за поддержку проекта.<br />
-    Благодаря вам колесо не останавливается.</caption>
-    <thead>
-        <tr>
-            <th>Друзья проекта</th>
-            <th>Назначение перевода</th>
-            <th>Сумма</th>
-            <th>Дата</th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-            <td>Алекс Алмэ Вейн</td>
-            <td>На приближение момента появления города Верус.</td>
-            <td>1&nbsp;000&nbsp;руб.</td>
-            <td>15.02.2016</td>
-        </tr>
-        <tr>
-            <td>Женя, просто Женя</td>
-            <td></td>
-            <td>150&nbsp;WMU</td>
-            <td>12.01.2016</td>
-        </tr>
-        <tr>
-            <td>Парамошкин Илья Олегович</td>
-            <td>На усмотрение Администрации</td>
-            <td>2&nbsp;000&nbsp;руб.</td>
-            <td>03.10.2015</td>
-        </tr>
-        <tr>
-            <td>Парамошкин Илья Олегович</td>
-            <td>Поддержка проекта RoFenrir</td>
-            <td>1&nbsp;050&nbsp;руб.</td>
-            <td>29.08.2015</td>
-        </tr>
-        <tr>
-            <td>Парамошкин Илья Олегович</td>
-            <td>Поддержка проекта RoFenrir</td>
-            <td>1&nbsp;300&nbsp;руб.</td>
-            <td>03.07.2015</td>
-        </tr>
-        <tr>
-            <td>Парамошкин Илья Олегович</td>
-            <td>Еще больше экспериментов!</td>
-            <td>1&nbsp;000&nbsp;руб.</td>
-            <td>01.06.2015</td>
-        </tr>
-        <tr>
-            <td>Почивалин Вадим Александрович</td>
-            <td>Больше экспериментов!</td>
-            <td>500&nbsp;руб.</td>
-            <td>29.05.2015</td>
-        </tr>
-        <tr>
-            <td rowspan="2">Парамошкин Илья Олегович</td>
-            <td>На печеньки</td>
-            <td>400 руб.</td>
-            <td rowspan="2">30.04.2015</td>
-        </tr>
-        <tr>
-            <td>На усмотрение Администрации</td>
-            <td>1&nbsp;000&nbsp;руб.</td>
-        </tr>
-        <tr>
-            <td>Почивалин Вадим Александрович</td>
-            <td>Поддержка экспериментов</td>
-            <td>500&nbsp;руб.</td>
-            <td>19.04.2015</td>
-        </tr>
-        <tr>
-            <td>Почивалин Вадим Александрович</td>
-            <td>На поддержку квестовой базы</td>
-            <td>800&nbsp;руб.</td>
-            <td>05.04.2015</td>
-        </tr>
-        <tr>
-            <td>Парамошкин Илья Олегович</td>
-            <td>На усмотрение Администрации</td>
-            <td>995&nbsp;руб.</td>
-            <td>03.04.2015</td>
-        </tr>
-        <tr>
-            <td>Парамошкин Илья Олегович</td>
-            <td>На усмотрение Администрации</td>
-            <td>1&nbsp;300&nbsp;руб.</td>
-            <td>18.12.2014</td>
-        </tr>
-    </tbody>
-</table>
-<p>*Вся информация опубликована только по обоюдному согласию и разрешению.</p>
+    <div class="body-content">
+
+        <div class="row">
+            <div class="col-lg-4">
+                <h2>Регистрация</h2>
+                <p>Регистрируйся на сайте и создавай учётные записи на игровом сервере. Один аккаунт управляет несколькими игровыми учётными записями. Количество учётных записей не ограничено.</p>
+                <p><a class="btn btn-default" href="http://cp.rofenrir.ru/">Регистрация</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Скачать клиент</h2>
+                <p>Скачай полный игровой клиент Ragnarok Online. Разархивируй куда хочешь и запусти Fenrir.exe</p>
+                <p><a class="btn btn-default" href="https://yadi.sk/d/oXW3HR5CmraSr">Скачать</a></p>
+            </div>
+            <div class="col-lg-4">
+                <h2>Радоваться</h2>
+                <p>Наслаждайтесь игрой. Если возникнут вопросы воспользуйтесь поиском по сайту или задайте вопрос на форуме. Наше доброжелательное сообщество всегда радо помочь новичкам.</p>
+                <p><a class="btn btn-default" href="http://forum.rofenrir.ru">Форум</a></p>
+            </div>
+        </div>
+
+    </div>
+</div>
