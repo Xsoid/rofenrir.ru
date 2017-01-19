06ca195a31ed91cd7f4282a35dd7e1f86a10a64e
Xsoid <xsoid@ya.ru>
2017-01-19 21:02:26 +0400

    fix

--- views/site/pages/ru/about/_menu.php
+++ views/site/pages/ru/about/_menu.php
@@ -1,8 +1,4 @@
-<div class="col-lg-3">
-    <div class="bs-component">
-        <div class="list-group pull-left">
-          <a href="index" class="list-group-item">Особенности сервера</a>
-          <a href="teh" class="list-group-item">Техкарта</a>
-        </div>
-    </div>
+<div class="list-group">
+  <a href="index" class="list-group-item">Особенности сервера</a>
+  <a href="teh" class="list-group-item">Техкарта</a>
 </div>
