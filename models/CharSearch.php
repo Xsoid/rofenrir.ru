c2e7ebfbb15db3ade0c97797a22f002c0ec76780
Alexader <aflade@ya.ru>
2018-05-16 15:18:33 +0400

    fix

--- models/CharSearch.php
+++ models/CharSearch.php
@@ -6,6 +6,7 @@ use Yii;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
 use app\models\Char;
+use yii\db\conditions\OrCondition;
 
 /**
  * CharSearch represents the model behind the search form of `app\models\Char`.
@@ -92,80 +93,86 @@ class CharSearch extends Char
         }
 
         // grid filtering conditions
-//        $query->andFilterWhere([
-//            'char.char_id' => $this->char_id,
-//            'char.account_id' => $this->account_id,
-//            'char.char_num' => $this->char_num,
-//            'char.base_level' => $this->base_level,
-//            'char.job_level' => $this->job_level,
-//            'char.base_exp' => $this->base_exp,
-//            'char.job_exp' => $this->job_exp,
-//            'char.zeny' => $this->zeny,
-//            'char.str' => $this->str,
-//            'char.agi' => $this->agi,
-//            'char.vit' => $this->vit,
-//            'char.int' => $this->int,
-//            'char.dex' => $this->dex,
-//            'char.luk' => $this->luk,
-//            'char.max_hp' => $this->max_hp,
-//            'char.hp' => $this->hp,
-//            'char.max_sp' => $this->max_sp,
-//            'char.sp' => $this->sp,
-//            'char.status_point' => $this->status_point,
-//            'char.skill_point' => $this->skill_point,
-//            'char.option' => $this->option,
-//            'char.karma' => $this->karma,
-//            'char.manner' => $this->manner,
-//            'char.party_id' => $this->party_id,
-//            'char.guild_id' => $this->guild_id,
-//            'char.pet_id' => $this->pet_id,
-//            'char.homun_id' => $this->homun_id,
-//            'char.hair' => $this->hair,
-//            'char.hair_color' => $this->hair_color,
-//            'char.clothes_color' => $this->clothes_color,
-//            'char.body' => $this->body,
-//            'char.weapon' => $this->weapon,
-//            'char.shield' => $this->shield,
-//            'char.head_top' => $this->head_top,
-//            'char.head_mid' => $this->head_mid,
-//            'char.head_bottom' => $this->head_bottom,
-//            'char.robe' => $this->robe,
-//            'char.last_x' => $this->last_x,
-//            'char.last_y' => $this->last_y,
-//            'char.save_x' => $this->save_x,
-//            'char.save_y' => $this->save_y,
-//            'char.partner_id' => $this->partner_id,
-//            'char.online' => $this->online,
-//            'char.father' => $this->father,
-//            'char.mother' => $this->mother,
-//            'char.child' => $this->child,
-//            'char.fame' => $this->fame,
-//            'char.rename' => $this->rename,
-//            'char.delete_date' => $this->delete_date,
-//            'char.elemental_id' => $this->elemental_id,
-//            'char.moves' => $this->moves,
-//            'char.unban_time' => $this->unban_time,
-//            'char.font' => $this->font,
-//            'char.uniqueitem_counter' => $this->uniqueitem_counter,
-//            'char.hotkey_rowshift' => $this->hotkey_rowshift,
-//            'char.clan_id' => $this->clan_id,
-//            'char.last_login' => $this->last_login,
-//        ]);
+        $query->andFilterWhere([
+            'char.char_id' => $this->char_id,
+            'char.account_id' => $this->account_id,
+            'char.char_num' => $this->char_num,
+            'char.base_level' => $this->base_level,
+            'char.job_level' => $this->job_level,
+            'char.base_exp' => $this->base_exp,
+            'char.job_exp' => $this->job_exp,
+            'char.zeny' => $this->zeny,
+            'char.str' => $this->str,
+            'char.agi' => $this->agi,
+            'char.vit' => $this->vit,
+            'char.int' => $this->int,
+            'char.dex' => $this->dex,
+            'char.luk' => $this->luk,
+            'char.max_hp' => $this->max_hp,
+            'char.hp' => $this->hp,
+            'char.max_sp' => $this->max_sp,
+            'char.sp' => $this->sp,
+            'char.status_point' => $this->status_point,
+            'char.skill_point' => $this->skill_point,
+            'char.option' => $this->option,
+            'char.karma' => $this->karma,
+            'char.manner' => $this->manner,
+            'char.party_id' => $this->party_id,
+            'char.guild_id' => $this->guild_id,
+            'char.pet_id' => $this->pet_id,
+            'char.homun_id' => $this->homun_id,
+            'char.hair' => $this->hair,
+            'char.hair_color' => $this->hair_color,
+            'char.clothes_color' => $this->clothes_color,
+            'char.body' => $this->body,
+            'char.weapon' => $this->weapon,
+            'char.shield' => $this->shield,
+            'char.head_top' => $this->head_top,
+            'char.head_mid' => $this->head_mid,
+            'char.head_bottom' => $this->head_bottom,
+            'char.robe' => $this->robe,
+            'char.last_x' => $this->last_x,
+            'char.last_y' => $this->last_y,
+            'char.save_x' => $this->save_x,
+            'char.save_y' => $this->save_y,
+            'char.partner_id' => $this->partner_id,
+            'char.online' => $this->online,
+            'char.father' => $this->father,
+            'char.mother' => $this->mother,
+            'char.child' => $this->child,
+            'char.fame' => $this->fame,
+            'char.rename' => $this->rename,
+            'char.delete_date' => $this->delete_date,
+            'char.elemental_id' => $this->elemental_id,
+            'char.moves' => $this->moves,
+            'char.unban_time' => $this->unban_time,
+            'char.font' => $this->font,
+            'char.uniqueitem_counter' => $this->uniqueitem_counter,
+            'char.hotkey_rowshift' => $this->hotkey_rowshift,
+            'char.clan_id' => $this->clan_id,
+            'char.last_login' => $this->last_login,
+        ]);
 
-        if ($this->is_online) {
-            $query->andFilterWhere(['char.online' => 1]);
-        }
-        if ($this->is_offline) {
-            $query->andFilterWhere(['char.online' => 0]);
-        }
-        if ($this->is_trade) {
-        }
+        $query->andFilterWhere(['like', 'char.name', $this->name])
+            ->andFilterWhere(['like', 'char.last_map', $this->last_map])
+            ->andFilterWhere(['like', 'char.save_map', $this->save_map])
+            ->andFilterWhere(['like', 'char.sex', $this->sex]);
+
         $query->andFilterWhere(['in', 'char.class', $this->class]);
 
-//        $query->andFilterWhere(['like', 'char.name', $this->name])
-//            ->andFilterWhere(['like', 'char.last_map', $this->last_map])
-//            ->andFilterWhere(['like', 'char.save_map', $this->save_map])
-//            ->andFilterWhere(['like', 'char.sex', $this->sex]);
+        $or = [];
+        if ($this->is_online) {
+            $or[] = ['char.online' => 1];
+        }
+        if ($this->is_offline) {
+            $or[] = ['char.online' => 0];
+        }
+        if ($this->is_trade) {
+            $query->andHaving(['>', 'vendingCount', 0]);
+        }
+        if (!empty($or)) {
+            $query->andOnCondition(new OrCondition($or));
+        }
 
         return $dataProvider;
     }
