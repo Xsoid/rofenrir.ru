b655592ef928848669653eab381a1f449ea69c38
Xsoid <xsoid@ya.ru>
2018-10-16 19:31:32 +0400

    Ссылка на патчи

--- views/site/pages/client.php
+++ views/site/pages/client.php
@@ -2,14 +2,14 @@
 
 /* @var $this yii\web\View */
 
-$this->title = 'Клиент Ragnarok Online';
+$this->title = 'Клиент Fenrir Ragnarok Online';
 $this->registerMetaTag([
      'name' => 'keywords',
      'content' => 'Ragnarok Online, Renewal, Fenrir, RoFenrir, сервер, бесплатная игра, клиент игры, полный клиент'
 ]);
 $this->registerMetaTag([
      'name' => 'description',
-     'content' => 'Скачать клиент для бесплатного игровой сервер Ragnarok Online. Нет абонентская платы и внутренних покупок. Игра без ограничений.'
+     'content' => 'Скачать клиент для бесплатного игровой сервер Fenrir Ragnarok Online. Нет абонентская платы и внутренних покупок. Игра без ограничений.'
 ]);
 $this->params['breadcrumbs'][] = $this->title;
 ?>
@@ -19,6 +19,7 @@ $this->params['breadcrumbs'][] = $this->title;
         <h1>Скачать клиент</h1>
         <p><small>Игра работает только на ПК под управлением Windows</small></p>
         <h3>Полный клиент RoFenrir 18.06</h3>
-        <p><a class="btn btn-success" href="https://yadi.sk/d/3ViWyv2sdGR2fw" target="_metrics">Скачать с Яндекс Диск</a>/p>
+        <p><a class="btn btn-success" href="https://yadi.sk/d/3ViWyv2sdGR2fw" target="_metrics">Скачать с Яндекс Диск</a></p>
+        Директория на Яндекс.Диске где будут выкладываться все клиенты и патчи: <a href="https://yadi.sk/d/tH_1TVsaR2j3wQ" target="_metrics">https://yadi.sk/d/tH_1TVsaR2j3wQ</a>
     </div>
 </div>
--- views/world/char.php
+++ views/world/char.php
@@ -21,82 +21,73 @@ $this->params['breadcrumbs'][] = 'Жители мир Fenrir';
 
 
 
-<div class="char-index">
-    <div class="row">
-        <div class="col-lg-9">
-            <p>Показывать:<br>            </p>
-
-            <?php $form = ActiveForm::begin([
-                'action' => ['char'],
-                'method' => 'get',
-            ]); ?>
-
-            <div class="form-group">
-                <?= $form->field($searchModel, 'is_online', [
-                    'options' => [
-                        'class' => 'checkbox-inline'
-                    ],
-                ])->checkbox() ?>
-                <?= $form->field($searchModel, 'is_offline', [
-                    'options' => [
-                        'class' => 'checkbox-inline'
-                    ],
-                ])->checkbox() ?>
-
-                <?= $form->field($searchModel, 'class', [
-                    'labelOptions' => [
-                        'class' => 'hidden',
-                    ],
-                ])->dropDownList(\app\models\Char::getClassList(), [
-                    'prompt' => 'Все профессии',
-                ]) ?>
-            </div>
-
-
-            <div class="form-group">
-                <?= Html::submitButton('Искать', ['class' => 'btn btn-primary']) ?>
-            </div>
-
-            <?php ActiveForm::end(); ?>
-
-            <h2>Жители мира Fenrir</h2>
-            <?= GridView::widget([
-                'dataProvider' => $dataProvider,
-//        'filterModel' => $searchModel,
-                'columns' => [
-//                    'char_id',
-//                    'account_id',
-//            'name',
-                    'title',
-                    'className',
-                    'base_level',
-                    'job_level',
-//            'guild_id',
-                    'last_map',
-//            'last_x',
-//            'last_y',
-//                    'online',
-//            'sex',
-//            'clan_id',
+<div class="row">
+    <div class="col-lg-9">
+        <p>Показывать:<br></p>
+        <?php $form = ActiveForm::begin([
+            'action' => ['char'],
+            'method' => 'get',
+        ]); ?>
+        <div class="form-group">
+            <?= $form->field($searchModel, 'is_online', [
+                'options' => [
+                    'class' => 'checkbox-inline'
                 ],
-                'rowOptions' => function($model) {
-                    $classNames = [];
-                    if ($model->online) {
-                        $classNames[] = 'success';
-                    }
-                    if ($model->vendingCount) {
-                        $classNames[] = 'warning';
-                    }
-                    return [
-                        'class' => implode(' ', $classNames),
-                    ];
-                }
-            ]); ?>
+            ])->checkbox() ?>
+            <?= $form->field($searchModel, 'is_offline', [
+                'options' => [
+                    'class' => 'checkbox-inline'
+                ],
+            ])->checkbox() ?>
+            <?= $form->field($searchModel, 'class', [
+                'labelOptions' => [
+                    'class' => 'hidden',
+                ],
+            ])->dropDownList(\app\models\Char::getClassList(), [
+                'prompt' => 'Все профессии',
+            ]) ?>
         </div>
-        <div class="col-lg-3">
-            <div class="bs-component">
-                <?= $this->render('_menu') ?>
-            </div>
+        <div class="form-group">
+            <?= Html::submitButton('Искать', ['class' => 'btn btn-primary']) ?>
+        </div>
+        <?php ActiveForm::end(); ?>
+        <h2>Жители мира Fenrir</h2>
+        <?= GridView::widget([
+            'dataProvider' => $dataProvider,
+            //'filterModel' => $searchModel,
+            'columns' => [
+        //        'char_id',
+        //        'account_id',
+        //'name',
+                'title',
+                'className',
+                'base_level',
+                'job_level',
+        //'guild_id',
+                'last_map',
+        //'last_x',
+        //'last_y',
+        //        'online',
+        //'sex',
+        //'clan_id',
+            ],
+            'rowOptions' => function($model) {
+                $classNames = [];
+                if ($model->online) {
+                    $classNames[] = 'success';
+                }
+                if ($model->vendingCount) {
+                    $classNames[] = 'warning';
+                }
+                return [
+                    'class' => implode(' ', $classNames),
+                ];
+            }
+        ]); ?>
+    </div>
+    <div class="col-lg-3">
+        <div class="bs-component">
+            <?= $this->render('_menu') ?>
         </div>
     </div>
 </div>
