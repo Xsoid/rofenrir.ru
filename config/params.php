15d2f7d191317006c22279ad7a6a1d889063be68
Alexand Hmelev <xsoid@ya.ru>
2019-09-17 10:17:13 +0400

    params

--- config/params.php
+++ config/params.php
@@ -5,4 +5,5 @@ return [
     'authPassword' => 'nmE6PtWHqcTYtoBcmPqzKppQ',
     'authUrl' => 'https://rpgid.ru/login',
     'profileUrl' => 'https://rpgid.ru/user/profile',
+    'authDomain' => 'https://rpgid.ru',
 ];
