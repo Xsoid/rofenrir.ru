98da237a81408e73900c04cf07a4f793335618cc
Alexand Hmelev <xsoid@ya.ru>
2020-05-13 15:36:44 +0400

    Авторизация через соц сети.

--- views/site/index.php
+++ views/site/index.php
@@ -12,10 +12,13 @@ $this->registerMetaTag([
     'content' => 'RoFenrir -  русский экспериментальный сервер Ragnarok Online. Бесплатный: без абонентской платы и внутренних платежей.'
 ]);
 ?>
-<div class="jumbotron col-12" style="background-image:url(img/character-summoner.jpg);background-position:center;">
-    <h1>RoFenrir</h1>
+<div class="jumbotron col-12 p-5" style="background-image:url(img/character-summoner.jpg);background-position:center;">
+    <h1 class="mt-0">RoFenrir</h1>
     <p class="lead">Ragnarok Online Server</p>
-    <p><a class="btn btn-lg btn-success" href="<?= $authUrl ?>">ВХОД</a></p>
+    <p><a class="btn btn-success btn-lg" style="font-size: 32px;" href="<?= $authUrl ?>">ВХОД</a><p>
+    <p><a href="<?= $authVkontakteUrl ?>" class="btn btn-success" title="Авторизация через VK"><i class="fab fa-vk" aria-hidden="true"></i></a> 
+        <a href="<?= $authYandex_oauthUrl ?>" class="btn btn-success" title="Авторизация через Яндекс"><i class="fab fa-yandex" aria-hidden="true"></i></a> 
+        <a href="<?= $authGoogle_oauthUrl ?>" class="btn btn-success" title="Авторизация через Google"><i class="fab fa-google" aria-hidden="true"></i></a></p>
     <p><small>У нас есть котики</small></p>
 </div>
 <div class="content">
@@ -23,7 +26,10 @@ $this->registerMetaTag([
         <div class="col">
             <h2>Регистрация</h2>
             <p>Регистрируйся на сайте и создавай учётные записи в игре. Один аккаунт RPG ID управляет несколькими игровыми учётными записями. Количество учётных записей не ограничено.</p>
-            <p><a class="btn btn-default" href="<?= $authUrl ?>">Регистрация</a></p>
+            <p><a class="btn btn-default" href="<?= $authUrl ?>">Регистрация</a> 
+            <a href="<?= $authVkontakteUrl ?>" class="btn btn-default" title="Быстрая регистрация через VK"><i class="fab fa-vk" aria-hidden="true"></i></a> 
+            <a href="<?= $authYandex_oauthUrl ?>" class="btn btn-default" title="Быстрая регистрация через Яндекс"><i class="fab fa-yandex" aria-hidden="true"></i></a> 
+            <a href="<?= $authGoogle_oauthUrl ?>" class="btn btn-default" title="Быстрая регистрация через Google"><i class="fab fa-google" aria-hidden="true"></i></a></p>
         </div>
         <div class="col">
             <h2>Скачать клиент</h2>
@@ -34,6 +40,10 @@ $this->registerMetaTag([
             <h2>Радоваться</h2>
             <p>Наслаждайтесь игрой. Присоединяйтесь к сообществам созданными нашими игроками.</p>
             <p><a class="btn btn-default" href="http://forum.rofenrir.ru">Форум</a> <a class="btn btn-default" href="https://discord.gg/WMNz35A" target="_metrics">Discord</a></p>
+        </div>
+        <div class="col-12">
+            <p><small>* Для быстрой регистрации можете воспользоваться любимой соц.сетью и авторизовываться через неё.<br>
+            ** В настройках профиля можно привязать к учетной записи все доступные соц.сети и автризовываться через любую из них.</small></p>
         </div>
     </div>
 </div>
