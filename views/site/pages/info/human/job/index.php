4c239cb9bf22085e31f39a58eb404f3eaaff2e2a
Alexand Hmelev <xsoid@ya.ru>
2020-05-17 15:23:04 +0400

    Job Upd

--- views/site/pages/info/human/job/index.php
+++ views/site/pages/info/human/job/index.php
@@ -21,264 +21,244 @@ $this->params['breadcrumbs'][] = 'Профессии';
   <tbody>
     <tr>
       <td scope="row" rowspan="19" valign="middle">
-        <img src="/img/content/info/job/sprite/0_1_1.gif">
-        <img src="/img/content/info/job/sprite/0_2_1.gif">
-        <h5>Novice</h5>
+        <img src="/img/content/info/job/sprite-gif/0.gif">
+        <h5>Новичок [Novice]</h5>
       </td>
       <td rowspan="2">
-        <img src="/img/content/info/job/sprite/1_1_1.gif">
-        <img src="/img/content/info/job/sprite/1_2_1.gif">
-        <h5>Swordman</h5>
+        <img src="/img/content/info/job/sprite-gif/1.gif">
+        <h5>Мечник [Swordman]</h5>
       </td>
       <td>
-        <img src="/img/content/info/job/sprite/7_1_1.gif">
-        <img src="/img/content/info/job/sprite/7_2_1.gif">
-        <h5>Knight</h5>
+        <img src="/img/content/info/job/sprite-gif/7.gif">
+        <h5>Рыцарь [Knight]</h5>
       </td>
       <td>
         <img src="/img/content/info/job/sprite/4008_1_1.gif">
         <img src="/img/content/info/job/sprite/4008_2_1.gif">
-        <h5>Lord Knight</h5>
+        <h5>Командор [Lord Knight]</h5>
       </td>
       <td>
         <img src="/img/content/info/job/sprite/4054_1_1.gif">
         <img src="/img/content/info/job/sprite/4054_2_1.gif">
-        <h5>Rune Knight</h5>
+        <h5>Рунмейстер [Rune Knight]</h5>
       </td>
     </tr>
     <tr>
       <td>
-        <img src="/img/content/info/job/sprite/14_1_1.gif">
-        <img src="/img/content/info/job/sprite/14_2_1.gif">
-        <h5>Crusader</h5>
+        <img src="/img/content/info/job/sprite-gif/14.gif">
+        <h5>Крестоносец [Crusader]</h5>
       </td>
       <td>
         <img src="/img/content/info/job/sprite/4015_1_1.gif">
         <img src="/img/content/info/job/sprite/4015_2_1.gif">
-        <h5>Paladin</h5>
+        <h5>Паладин [Paladin]</h5>
       </td>
       <td>
         <img src="/img/content/info/job/sprite/4066_1_1.gif">
         <img src="/img/content/info/job/sprite/4066_2_1.gif">
-        <h5>Royal Guard</h5>
+        <h5>Тамплиер [Royal Guard]</h5>
       </td>
     </tr>
     <tr>
       <td rowspan="2">
-        <img src="/img/content/info/job/sprite/2_1_1.gif">
-        <img src="/img/content/info/job/sprite/2_2_1.gif">
-        <h5>Magician</h5>
+        <img src="/img/content/info/job/sprite-gif/2.gif">
+        <h5>Маг [Magician]</h5>
       </td>
       <td>
-        <img src="/img/content/info/job/sprite/9_1_1.gif">
-        <img src="/img/content/info/job/sprite/9_2_1.gif">
-        <h5>Wizard</h5>
+        <img src="/img/content/info/job/sprite-gif/9.gif">
+        <h5>Волшебник [Wizard]</h5>
       </td>
       <td>
         <img src="/img/content/info/job/sprite/4010_1_1.gif">
         <img src="/img/content/info/job/sprite/4010_2_1.gif">
-        <h5>High Wizard</h5>
+        <h5>Архимаг [High Wizard]</h5>
       </td>
       <td>
         <img src="/img/content/info/job/sprite/4055_1_1.gif">
         <img src="/img/content/info/job/sprite/4055_2_1.gif">
-        <h5>Warlock</h5>
+        <h5>Чародей [Warlock]</h5>
       </td>
     </tr>
     <tr>
       <td>
-        <img src="/img/content/info/job/sprite/16_1_1.gif">
-        <img src="/img/content/info/job/sprite/16_2_1.gif">
-        <h5>Sage</h5>
+        <img src="/img/content/info/job/sprite-gif/16.gif">
+        <h5>Мудрец [Sage]</h5>
       </td>
       <td>
         <img src="/img/content/info/job/sprite/4017_1_1.gif">
         <img src="/img/content/info/job/sprite/4017_2_1.gif">
-        <h5>Professor</h5>
+        <h5>Профессор [Professor]</h5>
       </td>
       <td>
         <img src="/img/content/info/job/sprite/4067_1_1.gif">
         <img src="/img/content/info/job/sprite/4067_2_1.gif">
-        <h5>Sorcerer</h5>
+        <h5>Элементалист [Sorcerer]</h5>
       </td>
     </tr>
     <tr>
       <td rowspan="2">
-        <img src="/img/content/info/job/sprite/5_1_1.gif">
-        <img src="/img/content/info/job/sprite/5_2_1.gif">
-        <h5>Merchant</h5>
+        <img src="/img/content/info/job/sprite-gif/5.gif">
+        <h5>Торговец [Merchant]</h5>
       </td>
       <td>
-        <img src="/img/content/info/job/sprite/10_1_1.gif">
-        <img src="/img/content/info/job/sprite/10_2_1.gif">
-        <h5>Blacksmith</h5>
+        <img src="/img/content/info/job/sprite-gif/10.gif">
+        <h5>Кузнец [Blacksmith]</h5>
       </td>
       <td>
         <img src="/img/content/info/job/sprite/4011_1_1.gif">
         <img src="/img/content/info/job/sprite/4011_2_1.gif">
-        <h5>Whitesmith</h5>
+        <h5>Оружейник [Whitesmith]</h5>
       </td>
       <td>
         <img src="/img/content/info/job/sprite/4058_1_1.gif">
         <img src="/img/content/info/job/sprite/4058_2_1.gif">
-        <h5>Mechanic</h5>
+        <h5>Механик [Mechanic]</h5>
       </td>
     </tr>
     <tr>
       <td>
-        <img src="/img/content/info/job/sprite/18_1_1.gif">
-        <img src="/img/content/info/job/sprite/18_2_1.gif">
-        <h5>Alchemist</h5>
+        <img src="/img/content/info/job/sprite-gif/18.gif">
+        <h5>Алхимик [Alchemist]</h5>
       </td>
       <td>
         <img src="/img/content/info/job/sprite/4019_1_1.gif">
         <img src="/img/content/info/job/sprite/4019_2_1.gif">
-        <h5>Creator</h5>
+        <h5>Биохимик [Creator]</h5>
       </td>
       <td>
         <img src="/img/content/info/job/sprite/4071_1_1.gif">
         <img src="/img/content/info/job/sprite/4071_2_1.gif">
-        <h5>Genetic</h5>
+        <h5>Генетик [Genetic]</h5>
       </td>
     </tr>
     <tr>
       <td rowspan="2">
-        <img src="/img/content/info/job/sprite/4_1_1.gif">
-        <img src="/img/content/info/job/sprite/4_2_1.gif">
-        <h5>Acolyte</h5>
+        <img src="/img/content/info/job/sprite-gif/4.gif">
+        <h5>Послушник [Acolyte]</h5>
       </td>
       <td>
-        <img src="/img/content/info/job/sprite/8_1_1.gif">
-        <img src="/img/content/info/job/sprite/8_2_1.gif">
-        <h5>Priest</h5>
+        <img src="/img/content/info/job/sprite-gif/8.gif">
+        <h5>Священник [Priest]</h5>
       </td>
       <td>
         <img src="/img/content/info/job/sprite/4009_1_1.gif">
         <img src="/img/content/info/job/sprite/4009_2_1.gif">
-        <h5>High Priest</h5>
+        <h5>Епископ [High Priest]</h5>
       </td>
       <td>
         <img src="/img/content/info/job/sprite/4057_1_1.gif">
         <img src="/img/content/info/job/sprite/4057_2_1.gif">
-        <h5>Arch Bishop</h5>
+        <h5>Архиепископ [Arch Bishop]</h5>
       </td>
     </tr>
     <tr>
       <td>
-        <img src="/img/content/info/job/sprite/15_1_1.gif">
-        <img src="/img/content/info/job/sprite/15_2_1.gif">
-        <h5>Monk</h5>
+        <img src="/img/content/info/job/sprite-gif/15.gif">
+        <h5>Монах [Monk]</h5>
       </td>
       <td>
         <img src="/img/content/info/job/sprite/4016_1_1.gif">
         <img src="/img/content/info/job/sprite/4016_2_1.gif">
-        <h5>Champion</h5>
+        <h5>Мистик [Champion]</h5>
       </td>
       <td>
         <img src="/img/content/info/job/sprite/4070_1_1.gif">
         <img src="/img/content/info/job/sprite/4070_2_1.gif">
-        <h5>Sura</h5>
+        <h5>Отшельник [Sura]</h5>
       </td>
     </tr>
     <tr>
       <td rowspan="2">
-        <img src="/img/content/info/job/sprite/6_1_1.gif">
-        <img src="/img/content/info/job/sprite/6_2_1.gif">
-        <h5>Thief</h5>
+        <img src="/img/content/info/job/sprite-gif/6.gif">
+        <h5>Вор [Thief]</h5>
       </td>
       <td>
-        <img src="/img/content/info/job/sprite/12_1_1.gif">
-        <img src="/img/content/info/job/sprite/12_2_1.gif">
-        <h5>Assassin</h5>
+        <img src="/img/content/info/job/sprite-gif/12.gif">
+        <h5>Убийца [Assassin]</h5>
       </td>
       <td>
         <img src="/img/content/info/job/sprite/4013_1_1.gif">
         <img src="/img/content/info/job/sprite/4013_2_1.gif">
-        <h5>Assassin Cross</h5>
+        <h5>Ассасин [Assassin Cross]</h5>
       </td>
       <td>
         <img src="/img/content/info/job/sprite/4059_1_1.gif">
         <img src="/img/content/info/job/sprite/4059_2_1.gif">
-        <h5>Guillotine Cross</h5>
+        <h5>Каратель [Guillotine Cross]</h5>
       </td>
     </tr>
     <tr>
       <td>
-        <img src="/img/content/info/job/sprite/17_1_1.gif">
-        <img src="/img/content/info/job/sprite/17_2_1.gif">
-        <h5>Rogue</h5>
+        <img src="/img/content/info/job/sprite-gif/17.gif">
+        <h5>Разбойник [Rogue]</h5>
       </td>
       <td>
         <img src="/img/content/info/job/sprite/4018_1_1.gif">
         <img src="/img/content/info/job/sprite/4018_2_1.gif">
-        <h5>Stalker</h5>
+        <h5>Сталкер [Stalker]</h5>
       </td>
       <td>
         <img src="/img/content/info/job/sprite/4072_1_1.gif">
         <img src="/img/content/info/job/sprite/4072_2_1.gif">
-        <h5>Shadow Chaser</h5>
+        <h5>Преследователь [Shadow Chaser]</h5>
       </td>
     </tr>
     <tr>
       <td rowspan="3">
-        <img src="/img/content/info/job/sprite/3_1_1.gif">
-        <img src="/img/content/info/job/sprite/3_2_1.gif">
-        <h5>Archer</h5>
+        <img src="/img/content/info/job/sprite-gif/3.gif">
+        <h5>Лучник [Archer]</h5>
       </td>
       <td>
-        <img src="/img/content/info/job/sprite/11_1_1.gif">
-        <img src="/img/content/info/job/sprite/11_2_1.gif">
-        <h5>Hunter</h5>
+        <img src="/img/content/info/job/sprite-gif/11.gif">
+        <h5>Охотник [Hunter]</h5>
       </td>
       <td>
         <img src="/img/content/info/job/sprite/4012_1_1.gif">
         <img src="/img/content/info/job/sprite/4012_2_1.gif">
-        <h5>Sniper</h5>
+        <h5>Снайпер [Sniper]</h5>
       </td>
       <td>
         <img src="/img/content/info/job/sprite/4056_1_1.gif">
         <img src="/img/content/info/job/sprite/4056_2_1.gif">
-        <h5>Ranger</h5>
+        <h5>Рейнджер [Ranger]</h5>
       </td>
     </tr>
     <tr>
       <td>
-        <img src="/img/content/info/job/sprite/19_1_1.gif">
-        <h5>Bard</h5>
+        <img src="/img/content/info/job/sprite-gif/19.gif">
+        <h5>Бард [Bard]</h5>
       </td>
       <td>
         <img src="/img/content/info/job/sprite/4020_1_1.gif">
-        <h5>Clown</h5>
+        <h5>Менестрель [Clown]</h5>
       </td>
       <td>
         <img src="/img/content/info/job/sprite/4068_1_1.gif">
-        <h5>Minstrel</h5>
+        <h5>Маэстро [Minstrel]</h5>
       </td>
     </tr>
     <tr>
       <td>
-        <img src="/img/content/info/job/sprite/20_2_1.gif">
-        <h5>Dancer</h5>
+        <img src="/img/content/info/job/sprite-gif/20.gif">
+        <h5>Танцовщица [Dancer]</h5>
       </td>
       <td>
         <img src="/img/content/info/job/sprite/4021_2_1.gif">
-        <h5>Gypsy</h5>
+        <h5>Цыганка [Gypsy]</h5>
       </td>
       <td>
         <img src="/img/content/info/job/sprite/4069_2_1.gif">
-        <h5>Wanderer</h5>
+        <h5>Муза [Wanderer]</h5>
       </td>
     </tr>
     <tr>
       <td>
-        <img src="/img/content/info/job/sprite/23_1_1.gif">
-        <img src="/img/content/info/job/sprite/23_2_1.gif">
-        <h5>Super Novice</h5>
+        <img src="/img/content/info/job/sprite-gif/23.gif">
+        <h5>Супер Новичок<br>[Super Novice]</h5>
       </td>
       <td>
-        <img src="/img/content/info/job/sprite/4190_1_1.gif">
-        <img src="/img/content/info/job/sprite/4190_2_1.gif">
-        <h5>Ex. Super Novice</h5>
+        <img src="/img/content/info/job/sprite-gif/23.gif">
+        <h5>Экспертный Новичок<br>[Ex. Super Novice]</h5>
       </td>
       <td></td>
       <td></td>
@@ -287,17 +267,17 @@ $this->params['breadcrumbs'][] = 'Профессии';
       <td rowspan="2">
         <img src="/img/content/info/job/sprite/4046_1_1.gif">
         <img src="/img/content/info/job/sprite/4046_2_1.gif">
-        <h5>Taekwon</h5>
+        <h5>Таэквондист [Taekwon]</h5>
       </td>
       <td>
         <img src="/img/content/info/job/sprite/4047_1_1.gif">
         <img src="/img/content/info/job/sprite/4047_2_1.gif">
-        <h5>Star Gladiator</h5>
+        <h5>Гладиатор [Star Gladiator]</h5>
       </td>
       <td>
         <img src="/img/content/info/job/sprite/4239_1_1.gif">
         <img src="/img/content/info/job/sprite/4239_2_1.gif">
-        <h5>Скоро</h5>
+        <h5>Звёздный лорд</h5>
       </td>
       <td></td>
     </tr>
@@ -305,45 +285,43 @@ $this->params['breadcrumbs'][] = 'Профессии';
       <td>
         <img src="/img/content/info/job/sprite/4049_1_1.gif">
         <img src="/img/content/info/job/sprite/4049_2_1.gif">
-        <h5>Soul Linker</h5>
+        <h5>Медиум [Soul Linker]</h5>
       </td>
       <td>
         <img src="/img/content/info/job/sprite/4240_1_1.gif">
         <img src="/img/content/info/job/sprite/4240_2_1.gif">
-        <h5>Скоро</h5>
+        <h5>Жнец душ</h5>
       </td>
       <td></td>
     </tr>
       <td rowspan="2">
-        <img src="/img/content/info/job/sprite/25_1_1.gif">
-        <img src="/img/content/info/job/sprite/25_2_1.gif">
-        <h5>Ninja</h5>
+        <img src="/img/content/info/job/sprite-gif/25.gif">
+        <h5>Ниндзя [Ninja]</h5>
       </td>
       <td>
-        <img src="/img/content/info/job/sprite/4211_1_1.gif">
-        <h5>Kagerou</h5>
-      </td>
-      <td></td>
-      <td></td>
-    </tr>
-    <tr>
-      <td>
-        <img src="/img/content/info/job/sprite/4212_2_1.gif">
-        <h5>Oboro</h5>
+        <img src="/img/content/info/job/sprite-gif/4211.gif">
+        <h5>Кагеро [Kagerou]</h5>
       </td>
       <td></td>
       <td></td>
     </tr>
     <tr>
       <td>
-        <img src="/img/content/info/job/sprite/24_1_1.gif">
-        <img src="/img/content/info/job/sprite/24_2_1.gif">
-        <h5>Gunslinger</h5>
+        <img src="/img/content/info/job/sprite-gif/4212.gif">
+        <h5>Оборо [Oboro]</h5>
+      </td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td>
+        <img src="/img/content/info/job/sprite-gif/24.gif">
+        <h5>Стрелок [Gunslinger]</h5>
       </td>
       <td>
         <img src="/img/content/info/job/sprite/4215_1_1.gif">
         <img src="/img/content/info/job/sprite/4215_2_1.gif">
-        <h5>Rebellion</h5>
+        <h5>Мятежник [Rebellion]</h5>
       </td>
       <td></td>
       <td></td>
