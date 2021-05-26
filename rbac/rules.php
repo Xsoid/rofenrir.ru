68a97fa402ab65ad1ea7c6e45720130a2ab06ab8
Alexand Hmelev <xsoid@ya.ru>
2021-05-26 10:53:39 +0400

    fix

--- rbac/rules.php
+++ rbac/rules.php
@@ -1,6 +1,6 @@
 <?php
-return [
-    'userGroup' => 'O:22:"app\\rbac\\UserGroupRule":3:{s:4:"name";s:9:"userGroup";s:9:"createdAt";N;s:9:"updatedAt";N;}',
+$rules = [
+    'userRpgidGroup' => 'O:27:"app\\rbac\\UserRpgidGroupRule":3:{s:4:"name";s:14:"userRpgidGroup";s:9:"createdAt";N;s:9:"updatedAt";N;}',
 ];
 
 require_once(Yii::getAlias('@share/rbac/helper.php'));
