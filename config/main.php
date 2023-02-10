9272252efa88bd6b392d441a32c78b76267b6ca4
Alexand Hmelev <xsoid@ya.ru>
2023-02-10 23:07:57 +0400

    fix mail

--- config/main.php
+++ config/main.php
@@ -55,13 +55,13 @@ $config = [
                 ],
             ],
         ],
-        'mailer' => [
-            'class' => 'yii\swiftmailer\Mailer',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-            'useFileTransport' => true,
-        ],
+ //       'mailer' => [
+ //           'class' => 'yii\swiftmailer\Mailer',
+ //           // send all mails to a file by default. You have to set
+ //           // 'useFileTransport' to false and configure a transport
+ //           // for the mailer to send real emails.
+ //           'useFileTransport' => true,
+ //       ],
         'user' => [
             // 'class' => 'app\models\User',
             'identityClass' => 'app\models\User',
--- config/params.php
+++ config/params.php
@@ -1,7 +1,6 @@
 <?php
 
 return [
-    'adminEmail' => 'admin@example.com',
     'authPassword' => 'nmE6PtWHqcTYtoBcmPqzKppQ',
     'authUrl' => 'https://rpgid.ru/login',
     'profileUrl' => 'https://rpgid.ru/user/profile',
