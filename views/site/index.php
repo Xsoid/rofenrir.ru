04c6e59da134337b6f8d42369fade1ddc1814f45
Alexand Hmelev <xsoid@ya.ru>
2024-06-24 23:56:29 +0400

    no message

--- views/site/index.php
+++ views/site/index.php
@@ -16,7 +16,7 @@ $this->registerMetaTag([
     <h1 class="mt-0">RoFenrir</h1>
     <p class="lead"></p>
     <p><a class="btn btn-success btn-lg" style="font-size: 32px;" href="<?= $authUrl ?>">ВХОД</a><p>
-    <p><a href="<?= $authVkontakteUrl ?>" class="btn btn-success" title="Авторизация через VK"><i class="fab fa-vk" aria-hidden="true"></i></a> 
+    <p><a href="<?= $authVkId ?>" class="btn btn-success" title="Авторизация VK ID"><i class="fab fa-vk" aria-hidden="true"></i></a> 
         <a href="<?= $authYandex_oauthUrl ?>" class="btn btn-success" title="Авторизация через Яндекс"><i class="fab fa-yandex" aria-hidden="true"></i></a> 
         <a href="<?= $authGoogle_oauthUrl ?>" class="btn btn-success" title="Авторизация через Google"><i class="fab fa-google" aria-hidden="true"></i></a></p>
     <p><small>У нас есть котики</small></p>
@@ -27,7 +27,7 @@ $this->registerMetaTag([
             <h2>Регистрация</h2>
             <p>Регистрируйся на сайте и создавай учётные записи в игре. Один аккаунт RPG ID управляет несколькими игровыми учётными записями. Количество учётных записей не ограничено.</p>
             <p><a class="btn btn-default" href="<?= $authUrl ?>">Регистрация</a> 
-            <a href="<?= $authVkontakteUrl ?>" class="btn btn-default" title="Быстрая регистрация через VK"><i class="fab fa-vk" aria-hidden="true"></i></a> 
+            <a href="<?= $authVkId ?>" class="btn btn-default" title="Быстрая регистрация через VK ID"><i class="fab fa-vk" aria-hidden="true"></i></a> 
             <a href="<?= $authYandex_oauthUrl ?>" class="btn btn-default" title="Быстрая регистрация через Яндекс"><i class="fab fa-yandex" aria-hidden="true"></i></a> 
             <a href="<?= $authGoogle_oauthUrl ?>" class="btn btn-default" title="Быстрая регистрация через Google"><i class="fab fa-google" aria-hidden="true"></i></a></p>
         </div>
--- views/site/pages/client.php
+++ views/site/pages/client.php
@@ -17,27 +17,16 @@ $this->params['breadcrumbs'][] = $this->title;
 <div class="row">
     <div class="col">
         <h1>Файловые харнилища</h1>
-        <p>Актуальный клиент для <b>RoFenrir 18.06+</b><br>
+        <p>Актуальный клиент для <b>RoFenrir 21.11+</b><br>
         Хранилище содержит полный клиент RoFenrir и актуальные патчи если у вас есть альтернативнеы клиенты.</p>
         <p class="h4 pl-5">
             <a href="https://yadi.sk/d/2A1BDDUVciP4EA" target="_metrics">Хранилище на Яндекс.Диске</a><br>
-            <a href="https://mega.nz/#F!wZBDQITT!psbXseswyDxf2AjyWxpHcA" target="_metrics">Хранилище на MEGA</a><br>
-            <a href="https://1drv.ms/u/s!AqQBBXyzYdVhiJwaQKQw65f0syknow?e=BuUnEJ" target="_metrics">Хранилище на OneDrive</a>
         </p>
-        <!-- <div class="alert alert-info" role="alert">
-            Клиент обновлен 11.05.2020
-        </div> -->
         <p>Перед первым входом в игру запустите <b>Setup.exe</b> (рекомендуем запускать с правами Администратора) и укажите настройки игры который вам будут подходить. Обязательно сохраните изменения.</p>
         <p>Игра запускается через <b>Fenrir.exe</b></p>
     </div>
     <div class="col">
         <p class="h1">Системные требования</p>
-        <p>ОС Windows<br> 
-        Intel Pentium 500 MHz. CPU,<br> 
-        256MB RAM,<br> 
-        16MB видеопамяти,<br>
-        DirectX 8.0,<br> 
-        5GB+ HDD,<br> 
-        доступ в Интернет</p>
+        <p>ОС Windows</p>
     </div>
 </div>
